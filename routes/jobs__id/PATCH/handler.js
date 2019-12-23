8e8b7b03ae4a831486ffb8214ac65c3b31fa5c33
myarask <mark@yaraskavitch.com>
2019-12-22 21:21:51 -0500

    b

--- routes/jobs__id/PATCH/handler.js
+++ routes/jobs__id/PATCH/handler.js
@@ -1,8 +1,34 @@
 const crud = require('../../../services/crud');
+const stripe = require('../../../services/stripe');
 
-module.exports = table => request => {
+module.exports = table => async request => {
   const values = request.payload;
   const conditions = request.params;
+
+  if (values.status === 'reviewing') {
+    // Charge the requester for the job
+    const job = await crud(table).readOne(conditions);
+
+    const [requester, jobServices] = await Promise.all([
+      crud('requesters').readOne({ id: job.requesterId }),
+      crud('job_services').readAll({ jobId: job.id }),
+    ]);
+
+    const amount = jobServices.reduce((acc, obj) => acc + obj.flat_fee || 0, 0);
+
+    if (amount) {
+      const charge = await stripe.charges.create({
+        amount: amount * 1.13, // Charge HST
+        currency: 'cad',
+        customer: requester.customerId,
+      });
+
+      return crud(table).update(conditions, {
+        ...values,
+        chargeId: charge.id,
+      });
+    }
+  }
 
   return crud(table).update(conditions, values);
 };
--- routes/requesters/POST/handler.js
+++ routes/requesters/POST/handler.js
@@ -2,8 +2,13 @@ const crud = require('../../../services/crud');
 const stripe = require('../../../services/stripe');
 
 module.exports = table => async request => {
+  const { userId } = request.payload;
+  const user = await crud('users').readOne({ id: userId });
+
   const customer = await stripe.customers.create({
     source: 'tok_mastercard',
+    email: user.email,
+    description: [user.firstName, user.lastName].filter(Boolean).join(' '),
   });
 
   const content = {
