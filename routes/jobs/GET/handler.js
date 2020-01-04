0b72be2de612d98753d30b93cc760e648b480a5c
myarask <mark@yaraskavitch.com>
2020-01-04 14:33:32 -0500

    test

--- routes/jobs/GET/handler.js
+++ routes/jobs/GET/handler.js
@@ -1,15 +1,14 @@
 const validate = require('./validate');
+const axios = require('axios');
 const models = require('../../../models');
+const url = 'https://maps.googleapis.com/maps/api/distancematrix/json';
 
 module.exports = table => async request => {
   validate(request);
 
   const { statuses, offset, limit, lng, lat, ...conditions } = request.query;
 
-  if (lng !== undefined && lat !== undefined) {
-  }
-
-  const [data, count] = await Promise.all([
+  const [jobs, count] = await Promise.all([
     models.jobs
       .query()
       .where(conditions)
@@ -22,5 +21,28 @@ module.exports = table => async request => {
       .count('id')
       .first(),
   ]);
-  return { data, length: parseInt(count.count) };
+
+  if (lng !== undefined && lat !== undefined) {
+    const promises = jobs.reduce((acc, job) => {
+      const options = {
+        params: {
+          key: process.env.GOOGLE_KEY,
+          origins: `${lng},${lat}`,
+          destinations: `${job.lng},${job.lat}`,
+        },
+      };
+      const promise = axios.get(url, options).then(resp => {
+        job = {
+          ...job,
+          distance: resp.data.rows[0].elements[0].distance.text,
+          duration: resp.data.rows[0].elements[0].duration.text,
+        };
+      });
+      return [...acc, promise];
+    }, []);
+
+    await Promise.all(promises);
+  }
+
+  return { data: jobs, length: parseInt(count.count) };
 };
